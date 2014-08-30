3fe413194b53861ac7afdf3d34c25b542b612811
Mike Burton <mburton@groupon.com>
2014-08-30 12:27:57 -0700

    Fix warnings

--- TaskReminder/app/src/main/java/com/dummies/android/taskreminder/ReminderService.java
+++ TaskReminder/app/src/main/java/com/dummies/android/taskreminder/ReminderService.java
@@ -4,7 +4,6 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.util.Log;
 
 public class ReminderService extends WakeReminderIntentService {
 
@@ -14,8 +13,7 @@ public class ReminderService extends WakeReminderIntentService {
 
     @Override
     void doReminderWork(Intent intent) {
-        Log.d("ReminderService", "Doing work.");
-        Long rowId = intent.getExtras().getLong(ReminderProvider.COLUMN_ROWID);
+        long rowId = intent.getExtras().getLong(ReminderProvider.COLUMN_ROWID);
 
         NotificationManager mgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
@@ -25,21 +23,16 @@ public class ReminderService extends WakeReminderIntentService {
         PendingIntent pi = PendingIntent.getActivity(this, 0,
                 notificationIntent, PendingIntent.FLAG_ONE_SHOT);
 
-        Notification note = new Notification(
-                android.R.drawable.stat_sys_warning,
-                getString(R.string.notify_new_task_message),
-                System.currentTimeMillis());
-        note.setLatestEventInfo(this,
-                getString(R.string.notify_new_task_title),
-                getString(R.string.notify_new_task_message), pi);
-        note.defaults |= Notification.DEFAULT_SOUND;
-        note.flags |= Notification.FLAG_AUTO_CANCEL;
+        Notification note = new Notification.Builder(this)
+                .setContentTitle(getString(R.string.notify_new_task_title))
+                .setContentText(getString(R.string.notify_new_task_message))
+                .setSmallIcon(android.R.drawable.stat_sys_warning)
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
 
-        // An issue could occur if user ever enters over 2,147,483,647 tasks.
-        // (Max int value).
-        // I highly doubt this will ever happen. But is good to note.
-        int id = (int) ((long) rowId);
-        mgr.notify(id, note);
+        // An issue could occur if user ever enters over 2,147,483,647 (max int value) tasks.
+        mgr.notify((int) rowId, note);
 
     }
 }
