dce96f9266892a989bacf09ed9c2514393b1df6c
Mike Burton <mburton@groupon.com>
2014-08-30 15:52:56 -0700

    cleanup and comments

--- TaskReminder/app/src/main/java/com/dummies/android/taskreminder/TaskPreferencesFragment.java
+++ TaskReminder/app/src/main/java/com/dummies/android/taskreminder/TaskPreferencesFragment.java
@@ -5,13 +5,19 @@ import android.preference.EditTextPreference;
 import android.preference.PreferenceFragment;
 import android.text.method.DigitsKeyListener;
 
+/**
+ * A fragment that reads a list of preferences from a file and shows
+ * them to the user for editing.
+ */
 public class TaskPreferencesFragment extends PreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Construct the preferences screen from the XML config
         addPreferencesFromResource(R.xml.task_preferences);
 
-        // Set the time default to a numeric number only
+        // Use the number keyboard when editing the time preference
         EditTextPreference timeDefault = (EditTextPreference)
                 findPreference(getString(R.string
                         .pref_default_time_from_now_key));
