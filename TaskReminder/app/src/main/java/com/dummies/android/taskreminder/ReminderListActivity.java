2610346175a0462ff6bac7ed1b89349c72f8cc6c
Mike Burton <mburton@groupon.com>
2014-08-30 15:21:38 -0700

    cleanup and comments

--- TaskReminder/app/src/main/java/com/dummies/android/taskreminder/ReminderListActivity.java
+++ TaskReminder/app/src/main/java/com/dummies/android/taskreminder/ReminderListActivity.java
@@ -8,13 +8,16 @@ public class ReminderListActivity extends SingleFragmentActivity
 
     @Override
     protected Fragment newFragmentInstance() {
+        // Create a new ReminderListFragment when requested.
+        // This fragment doesn't need any params when it's created
         return new ReminderListFragment();
     }
 
     @Override
     public void editReminder(long id) {
+        // When we are asked to edit a reminder, start the
+        // ReminderEditActivity
         startActivity(new Intent(this, ReminderEditActivity.class)
-                .putExtra(
-                        ReminderProvider.COLUMN_TASKID, id));
+                .putExtra(ReminderProvider.COLUMN_TASKID, id));
     }
 }
