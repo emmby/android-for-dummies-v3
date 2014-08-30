ff19a9797bd09e85d4ef4bc4fbf65dacb26a57f2
Mike Burton <mburton@groupon.com>
2014-08-30 14:05:39 -0700

    Simplify alarm handling

--- TaskReminder/app/src/main/java/com/dummies/android/taskreminder/OnAlarmReceiver.java
+++ TaskReminder/app/src/main/java/com/dummies/android/taskreminder/OnAlarmReceiver.java
@@ -1,26 +1,47 @@
 package com.dummies.android.taskreminder;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ComponentInfo;
-import android.util.Log;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class OnAlarmReceiver extends BroadcastReceiver {
-
-    private static final String TAG = ComponentInfo.class.getCanonicalName();
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(TAG, "Received wake up from alarm manager.");
+        // Note: Do not do any asynchronous operations in BroadcastReceive.onReceive.
+        // If you need to do asynchronous operations (eg. network requests,
+        // disk or database reads or writes, etc.), then update OnAlarmReceiver
+        // to subclass android.support.v4.content.WakefulBroadcastReceiver
+        // and create a new service to do all of your heavy lifting.  Remember
+        // to call startWakefulService to start your service, and remember to
+        // call WakefulBroadcastReceiver.completeWakefulIntent from your service
+        // when you are done.
 
-        long rowid = intent.getExtras().getLong(ReminderProvider.COLUMN_ROWID);
+        long rowId = intent.getExtras().getLong(ReminderProvider.COLUMN_ROWID);
 
-        WakeReminderIntentService.acquireStaticLock(context);
+        NotificationManager mgr = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
 
-        Intent i = new Intent(context, ReminderService.class);
-        i.putExtra(ReminderProvider.COLUMN_ROWID, rowid);
-        context.startService(i);
+        Intent notificationIntent = new Intent(context, ReminderEditActivity.class);
+        notificationIntent.putExtra(ReminderProvider.COLUMN_ROWID, rowId);
+
+        PendingIntent pi = PendingIntent.getActivity(context, 0,
+                notificationIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        Notification note = new Notification.Builder(context)
+                .setContentTitle(context.getString(R.string.notify_new_task_title))
+                .setContentText(context.getString(R.string.notify_new_task_message))
+                .setSmallIcon(android.R.drawable.stat_sys_warning)
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+
+        // An issue could occur if user ever enters over 2,147,483,647 (max int value) tasks.
+        mgr.notify((int) rowId, note);
+
 
     }
 }
