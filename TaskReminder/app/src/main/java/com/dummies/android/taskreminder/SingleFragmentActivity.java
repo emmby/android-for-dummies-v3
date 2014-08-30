13ea679905067bad3615037ad2cc5e275de8fea5
Mike Burton <mburton@groupon.com>
2014-08-30 15:51:18 -0700

    cleanup and comments

--- TaskReminder/app/src/main/java/com/dummies/android/taskreminder/SingleFragmentActivity.java
+++ TaskReminder/app/src/main/java/com/dummies/android/taskreminder/SingleFragmentActivity.java
@@ -4,18 +4,27 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 
+/**
+ * A simple wrapper for showing a single fragment.
+ * Does not support adding or removing fragments from the activity.
+ */
 abstract public class SingleFragmentActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // If not already added to the Fragment manager add it. If you
-        // don't do this a new Fragment will be added every time this
-        // method is called (such as on orientation change)
+        // Add the fragment if it has not already been added to the
+        // FragmentManager. If you don't do this a new Fragment will be
+        // added every time this method is called (such as on
+        // orientation change)
         if (savedInstanceState == null)
             getFragmentManager().beginTransaction().add(
                     android.R.id.content, newFragmentInstance()).commit();
     }
 
+    /**
+     * Implement this method to create a new instance of the Fragment
+     * you wish to wrap.
+     */
     protected abstract Fragment newFragmentInstance();
 }
