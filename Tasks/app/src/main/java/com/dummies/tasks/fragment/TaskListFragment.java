afee71474dbd4bc14cff2d1b18dec25b98171e80
Mike Burton <mburton@groupon.com>
2014-08-30 18:48:46 -0700

    First pass at using RecyclerView

--- Tasks/app/src/main/java/com/dummies/tasks/fragment/TaskListFragment.java
+++ Tasks/app/src/main/java/com/dummies/tasks/fragment/TaskListFragment.java
@@ -1,6 +1,6 @@
 package com.dummies.tasks.fragment;
 
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.app.LoaderManager;
 import android.content.ContentUris;
 import android.content.CursorLoader;
@@ -8,53 +8,59 @@ import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView.AdapterContextMenuInfo;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import com.dummies.tasks.R;
 import com.dummies.tasks.activity.TaskPreferencesActivity;
+import com.dummies.tasks.interfaces.OnEditTask;
 
-public class TaskListFragment extends ListFragment implements
+import static com.dummies.tasks.provider.TaskProvider.COLUMN_TASKID;
+import static com.dummies.tasks.provider.TaskProvider.COLUMN_TITLE;
+import static com.dummies.tasks.provider.TaskProvider.CONTENT_URI;
+
+public class TaskListFragment extends Fragment implements
         LoaderManager.LoaderCallbacks<Cursor> {
 
-    private SimpleCursorAdapter adapter;
+    TaskListAdapter adapter;
+    RecyclerView recyclerView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Create an array to specify the fields we want to display in
-        // the list
-        // (only TITLE)
-        String[] from = new String[]{com.dummies.tasks.provider
-                .TaskProvider.COLUMN_TITLE};
-
-        // and an array of the fields we want to bind those fields to
-        // (in this
-        // case just text1)
-        int[] to = new int[]{R.id.text1};
-
-        // Now create a simple cursor adapter and set it to display
-        adapter = new SimpleCursorAdapter(getActivity(),
-                R.layout.task_row, null, from, to, 0);
-        setListAdapter(adapter);
+        adapter = new TaskListAdapter();
 
         getLoaderManager().initLoader(0, null, this);
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View v = inflater.inflate(R.layout.task_list_fragment,
+                container, false);
+        recyclerView = (RecyclerView) v.findViewById(R.id.recycler);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return v;
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        setEmptyText(getResources().getString(R.string.no_tasks));
-        registerForContextMenu(getListView());
         setHasOptionsMenu(true);
     }
 
@@ -69,7 +75,7 @@ public class TaskListFragment extends ListFragment implements
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_insert:
-                ((com.dummies.tasks.interfaces.OnEditTask) getActivity()).editTask(0);
+                ((OnEditTask) getActivity()).editTask(0);
                 return true;
             case R.id.menu_settings:
                 startActivity(new Intent(getActivity(),
@@ -81,22 +87,13 @@ public class TaskListFragment extends ListFragment implements
     }
 
     @Override
-    public void onListItemClick(ListView l, View v, int position,
-                                long id) {
-        super.onListItemClick(l, v, position, id);
-        ((com.dummies.tasks.interfaces.OnEditTask) getActivity()).editTask(id);
-    }
-
-    @Override
     public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_delete:
-                AdapterContextMenuInfo info = (AdapterContextMenuInfo)
-                        item
-                        .getMenuInfo();
+                AdapterContextMenuInfo info =
+                        (AdapterContextMenuInfo) item.getMenuInfo();
                 getActivity().getContentResolver().delete(
-                        ContentUris.withAppendedId(com.dummies.tasks.provider.TaskProvider
-                                        .CONTENT_URI,
+                        ContentUris.withAppendedId(CONTENT_URI,
                                 info.id), null, null);
                 return true;
         }
@@ -113,9 +110,8 @@ public class TaskListFragment extends ListFragment implements
 
     @Override
     public Loader<Cursor> onCreateLoader(int ignored, final Bundle args) {
-        return new CursorLoader(getActivity(),
-                com.dummies.tasks.provider.TaskProvider.CONTENT_URI,
-                null, null, null, null);
+        return new CursorLoader(getActivity(), CONTENT_URI, null, null,
+                null, null);
     }
 
     @Override
@@ -130,5 +126,69 @@ public class TaskListFragment extends ListFragment implements
         // above is about to be closed. We need to make sure we are no
         // longer using it.
         adapter.swapCursor(null);
+    }
+}
+
+class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.ViewHolder> {
+    Cursor cursor;
+    int titleColumnIndex;
+    int idColumnIndex;
+
+
+    public void swapCursor(Cursor c) {
+        cursor = c;
+        cursor.moveToFirst();
+        titleColumnIndex = cursor.getColumnIndex(COLUMN_TITLE);
+        idColumnIndex = cursor.getColumnIndex(COLUMN_TASKID);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_row, parent, false);
+
+        // wrap it in a ViewHolder
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder viewHolder,
+                                 final int i) {
+        // set the text
+        cursor.moveToPosition(i);
+        viewHolder.title.setText(cursor.getString(titleColumnIndex));
+
+        // Set the click action
+        viewHolder.title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long id = getItemId(i);
+                ((OnEditTask) viewHolder.title.getContext()).editTask(id);
+            }
+        });
+
+        //registerForContextMenu(viewHolder.title); // TODO not working
+    }
+
+    @Override
+    public long getItemId(int position) {
+        cursor.moveToPosition(position);
+        return cursor.getLong(idColumnIndex);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cursor!=null ? cursor.getCount() : 0;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+
+        public ViewHolder(TextView itemView) {
+            super(itemView);
+            title = itemView;
+        }
+
     }
 }
