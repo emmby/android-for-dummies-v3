d9b040ae082cc8b9492411e14e1090c3d16eef29
Mike Burton <mburton@groupon.com>
2014-09-15 20:13:41 -0700

    Simplify SilentModeToggle

--- SilentModeToggle/src/main/java/com/dummies/silentmodetoggle/MainActivity.java
+++ SilentModeToggle/src/main/java/com/dummies/silentmodetoggle/MainActivity.java
@@ -1,7 +1,6 @@
 package com.dummies.silentmodetoggle;
 
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,78 +10,66 @@ import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
-	private AudioManager mAudioManager;
-	private boolean mPhoneIsSilent;
+    AudioManager audioManager;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
 
-		checkIfPhoneIsSilent();
+        Button toggleButton = (Button) findViewById(R.id.toggleButton);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (isPhoneSilent()) {
+                    // Change back to normal mode
+                    audioManager.setRingerMode(
+                            AudioManager.RINGER_MODE_NORMAL);
+                } else {
+                    // Change to silent mode
+                    audioManager.setRingerMode(
+                            AudioManager.RINGER_MODE_SILENT);
+                }
 
-		setButtonClickListener();
+                // Now toggle the UI again
+                updateUi();
+            }
+        });
 
-		Log.d("SilentModeApp", "This is a test");
-	}
+        Log.d("SilentModeApp", "This is a test");
+    }
 
-	private void setButtonClickListener() {
-		Button toggleButton = (Button) findViewById(R.id.toggleButton);
-		toggleButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				if (mPhoneIsSilent) {
-					// Change back to normal mode
-					mAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-					mPhoneIsSilent = false;
-				} else {
-					// Change to silent mode
-					mAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-					mPhoneIsSilent = true;
-				}
+    /**
+     * Returns whether the phone is currently in silent mode.
+     */
+    private boolean isPhoneSilent() {
+        return audioManager.getRingerMode()
+                == AudioManager.RINGER_MODE_SILENT;
+    }
 
-				// Now toggle the UI again
-				toggleUi();
-			}
-		});
-	}
+    /**
+     * Updates the UI image to show silent or normal, as appropriate
+     */
+    private void updateUi() {
+        ImageView imageView = (ImageView) findViewById(R.id.phone_icon);
 
-	/**
-	 * Checks to see if the phone is currently in silent mode.
-	 */
-	private void checkIfPhoneIsSilent() {
-		int ringerMode = mAudioManager.getRingerMode();
-		if (ringerMode == AudioManager.RINGER_MODE_SILENT) {
-			mPhoneIsSilent = true;
-		} else {
-			mPhoneIsSilent = false;
-		}
+        int phoneImage = isPhoneSilent()
+                ? R.drawable.phone_on
+                : R.drawable.phone_silent;
 
-	}
+        imageView.setImageDrawable(getDrawable(phoneImage));
+    }
 
-	/**
-	 * Toggles the UI images from silent to normal and vice versa.
-	 */
-	private void toggleUi() {
-
-		ImageView imageView = (ImageView) findViewById(R.id.phone_icon);
-		Drawable newPhoneImage;
-
-		if (mPhoneIsSilent) {
-			newPhoneImage = getResources().getDrawable(R.drawable.phone_silent);
-
-		} else {
-			newPhoneImage = getResources().getDrawable(R.drawable.phone_on);
-		}
-
-		imageView.setImageDrawable(newPhoneImage);
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		checkIfPhoneIsSilent();
-		toggleUi();
-	};
+    /**
+     * Every time the activity is resumed, make sure to update the
+     * buttons to reflect the current state of the system (since the
+     * user may have changed the phone's silent state while we were in
+     * the background).
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateUi();
+    }
 }
