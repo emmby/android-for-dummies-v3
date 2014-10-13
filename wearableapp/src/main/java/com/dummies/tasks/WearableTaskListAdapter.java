a472982c8fabe904926c1d35b80db96720e22f96
Mike Burton <mb-github@niskala.org>
2014-10-12 17:48:06 -0700

    Now syncs with the phone

--- wearableapp/src/main/java/com/dummies/tasks/WearableTaskListAdapter.java
+++ wearableapp/src/main/java/com/dummies/tasks/WearableTaskListAdapter.java
@@ -1,4 +1,4 @@
-package com.dummies.wearable;
+package com.dummies.tasks;
 
 import android.content.Context;
 import android.support.wearable.view.WearableListView;
@@ -9,12 +9,13 @@ import android.widget.TextView;
 
 import com.google.android.gms.wearable.DataItem;
 import com.google.android.gms.wearable.DataMap;
-import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.DataMapItem;
 
 import java.util.List;
 
 public class WearableTaskListAdapter
-        extends WearableListView.Adapter {
+    extends WearableListView.Adapter
+{
 
     static final java.lang.String COLUMN_TITLE = "title";
 
@@ -26,25 +27,28 @@ public class WearableTaskListAdapter
     }
 
     @Override
-    public WearableListView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    public WearableListView.ViewHolder onCreateViewHolder(ViewGroup
+                                                                  viewGroup, int i) {
         return new ViewHolder(
-                inflater.inflate(R.layout.item_task, null));
+            inflater.inflate(R.layout.item_task, null));
     }
 
     @Override
-    public void onBindViewHolder(WearableListView.ViewHolder
-                                             viewHolder, int i) {
+    public void onBindViewHolder(
+        WearableListView.ViewHolder
+            viewHolder, int i) {
 
-        DataMap map = ((PutDataMapRequest)dataItems.get(i)).getDataMap();
+        DataMap map
+            = DataMapItem.fromDataItem(dataItems.get(i)).getDataMap();
 
-        ((ViewHolder)viewHolder).titleView.setText(
-                map.getString(COLUMN_TITLE)
+        ((ViewHolder) viewHolder).titleView.setText(
+            map.getString(COLUMN_TITLE)
         );
     }
 
     @Override
     public int getItemCount() {
-        return dataItems!=null ? dataItems.size() : 0;
+        return dataItems != null ? dataItems.size() : 0;
     }
 
     public void setResults(List<DataItem> dataItems) {
