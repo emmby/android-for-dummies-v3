84b9ed09ca8ef0a225f69e300f499f26db18ecb3
Mike Burton <mb-github@niskala.org>
2015-01-17 14:42:21 -0800

    Add a testcase to click phone icon

--- SilentModeToggle/src/androidTest/java/SilentModeToggleEspressoTest.java
+++ SilentModeToggle/src/androidTest/java/SilentModeToggleEspressoTest.java
@@ -4,13 +4,18 @@
 
 
 
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import com.dummies.silentmodetoggle.MainActivity;
 import com.dummies.silentmodetoggle.R;
+import com.dummies.silentmodetoggle.util.RingerHelper;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions
     .matches;
 import static android.support.test.espresso.matcher.ViewMatchers
@@ -21,6 +26,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 public class SilentModeToggleEspressoTest 
     extends ActivityInstrumentationTestCase2<MainActivity> 
 {
+    
+    AudioManager audioManager;
 
     public SilentModeToggleEspressoTest() {
         super(MainActivity.class);
@@ -33,10 +40,33 @@ public class SilentModeToggleEspressoTest
         // Get the Activity under test, starting it if necessary.
         // This must be done before you can run any testcases
         // that use the activity.
-        getActivity();
+        Activity activity = getActivity();
+        
+        // Retrieve an AudioManager from the activity
+        audioManager = (AudioManager) 
+            activity.getSystemService(Context.AUDIO_SERVICE);
     }
 
     public void testPhoneIconIsDisplayed() {
+        // When the phone_icon view is available,
+        // check that it is displayed.
         onView(withId(R.id.phone_icon)).check(matches(isDisplayed()));
+    }
+    
+    public void testCanToggleIcon() {
+        // When the phone_icon view is available,
+        // click it until the phone is no longer silent
+        while( RingerHelper.isPhoneSilent(audioManager) )
+            onView(withId(R.id.phone_icon)).perform(click());
+
+        // Sanity check that we're not in silent mode
+        assertTrue(!RingerHelper.isPhoneSilent(audioManager));
+
+        // When the phone_icon view is available,
+        // click it one more time.
+        onView(withId(R.id.phone_icon)).perform(click());
+        
+        // Then assert that the phone is now in silent mode.
+        assertTrue(RingerHelper.isPhoneSilent(audioManager));
     }
 }
